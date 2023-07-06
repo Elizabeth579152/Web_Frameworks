a533060c08be686a02bd183c802b349fdc742d67
Elizabeth Kwarteng <kwartenge2002@gmail.com>
2023-07-06 18:54:00 +0000

    app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)                    # this gets the name of the file so Fla
 @app.route("/")                          # this tells you the URL the method below is related to
 def hello_world():
     return "<p>Hello, World!</p>"        # this prints HTML to the webpage
-  
+
+ 
 if __name__ == '__main__':               # this should always be at the end
     app.run(debug=True, host="0.0.0.0")
