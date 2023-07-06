01891a06f23a631a2571f2ef76db42843c6c43f5
Elizabeth Kwarteng <kwartenge2002@gmail.com>
2023-07-06 19:06:35 +0000

    add path

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
+
 from flask import Flask
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
 
@@ -5,6 +8,14 @@ app = Flask(__name__)                    # this gets the name of the file so Fla
 def hello_world():
     return "<p>Hello, World!</p>"        # this prints HTML to the webpage
 
- 
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/Elizabeth152/Web_Frameworks')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 if __name__ == '__main__':               # this should always be at the end
     app.run(debug=True, host="0.0.0.0")
